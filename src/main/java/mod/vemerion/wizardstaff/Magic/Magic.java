4d46f5ef5007ad96d36d6e35f191537df0bb8041
vemerion <vemerion@gmail.com>
2020-09-28 18:43:25 +0200

    Fix bug where spell would drain hp while still having xp left
    This bug occured due to how vanilla implements the exp system via
    player.experience, player.experienceLevel, and player.totalExperience.
    The bug was fixed by using the player.giveExperiencePoints() only and
    without extra checks to see if experience was positive etc. Also, a
    method debt() was added that calculates how much (if any) damage a
    player should take when using a spell.

--- src/main/java/mod/vemerion/wizardstaff/Magic/Magic.java
+++ src/main/java/mod/vemerion/wizardstaff/Magic/Magic.java
@@ -9,38 +9,44 @@ import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 
 public abstract class Magic {
-	
+
 	protected static final int HOUR = 72000;
-	
+
 	protected float soundPitch(PlayerEntity player) {
 		return 0.8f + player.getRNG().nextFloat() * 0.4f;
 	}
-	
-	// FIXME: This only works in some cases due to how wierd the vanilla exp system is implemented
+
 	protected void cost(PlayerEntity player, double amount) {
 		int whole = Experience.add(player, amount);
-		int exp = player.experienceTotal;
+		double debt = debt(player, whole);
+		player.giveExperiencePoints(-whole);
+		if (debt > 0)
+			player.attackEntityFrom(DamageSource.MAGIC, (float) debt);
 
-		if (exp < whole) {
-			player.giveExperiencePoints(-exp);
-			whole -= exp;
-			player.attackEntityFrom(DamageSource.MAGIC, whole);
-		} else {
-			player.giveExperiencePoints(-whole);
-		}
 	}
-	
+
+	private double debt(PlayerEntity player, double amount) {
+		int trueLevel = player.experienceLevel;
+		amount -= player.experience * player.xpBarCap();
+		while (--player.experienceLevel >= 0 && amount > 0) {
+			amount -= player.xpBarCap();
+		}
+		player.experienceLevel = trueLevel;
+		return amount;
+	}
+
 	public abstract int getUseDuration(ItemStack staff);
+
 	public abstract boolean isMagicItem(Item item);
+
 	public abstract RenderMagic renderer();
 
-	
 	public void magicStart(World world, PlayerEntity player, ItemStack staff) {
 	}
-	
+
 	public void magicTick(World world, PlayerEntity player, ItemStack staff, int count) {
 	}
-	
+
 	public ItemStack magicFinish(World world, PlayerEntity player, ItemStack staff) {
 		return staff;
 	}
