65656fb6e50931bcd5865cbdf4a1e123cce97032
vemerion <vemerion@gmail.com>
2020-12-31 17:08:16 +0100

    Add pages to spellbook

--- src/main/java/mod/vemerion/wizardstaff/staff/SpellbookGui.java
+++ src/main/java/mod/vemerion/wizardstaff/staff/SpellbookGui.java
@@ -6,32 +6,41 @@ import java.util.List;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 
+import mod.vemerion.wizardstaff.Main;
 import mod.vemerion.wizardstaff.Magic.Magics;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.AbstractGui;
 import net.minecraft.client.gui.IGuiEventListener;
 import net.minecraft.client.gui.IRenderable;
 import net.minecraft.client.gui.widget.button.AbstractButton;
+import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.client.gui.widget.button.ImageButton;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.fml.client.gui.GuiUtils;
 
 public class SpellbookGui extends AbstractGui implements IRenderable, IGuiEventListener {
 	private static final ResourceLocation GUI = new ResourceLocation("textures/gui/recipe_book.png");
+	private static final ResourceLocation BUTTON = new ResourceLocation(Main.MODID, "textures/gui/spellbook.png");
 	private static final int X_OFFSET = 86;
 	private static final int X_SIZE = 147;
 	private static final int Y_SIZE = 166;
 	private static final int BORDER_X = 16;
-	private static final int BORDER_Y = 12;
-	private static final int BUTTON_SIZE = 20;
+	private static final int BORDER_Y = 10;
+	private static final int ITEM_SIZE = 20;
 	private static final int ITEMS_PER_ROW = 6;
 	private static final int ITEMS_PER_COLUMN = 7;
 	private static final int ITEMS_PER_PAGE = ITEMS_PER_ROW * ITEMS_PER_COLUMN;
+	private static final int BUTTON_SIZE = 10;
 
 	private boolean isActive;
 	private int width;
 	private int height;
 	private List<ItemButton> buttons;
+	private Button next;
+	private Button prev;
+	private int page;
 
 	public SpellbookGui() {
 	}
@@ -40,34 +49,65 @@ public class SpellbookGui extends AbstractGui implements IRenderable, IGuiEventL
 		this.width = width;
 		this.height = height;
 
-		int left = (width - X_SIZE) / 2 - X_OFFSET + BORDER_X;
-		int top = (height - Y_SIZE) / 2 + BORDER_Y;
+		int left = (width - X_SIZE) / 2 - X_OFFSET;
+		int top = (height - Y_SIZE) / 2;
+		int bottom = top + Y_SIZE;
+		int right = left + X_SIZE;
 
 		this.buttons = new ArrayList<>();
 		int i = 0;
 		for (ItemStack stack : Magics.getInstance().getMagicItems()) {
-			buttons.add(new ItemButton(left + (i % ITEMS_PER_ROW) * BUTTON_SIZE,
-					top + (i / ITEMS_PER_ROW) * BUTTON_SIZE, BUTTON_SIZE, BUTTON_SIZE, stack));
-			i++;
+			buttons.add(new ItemButton(left + BORDER_X + (i % ITEMS_PER_ROW) * ITEM_SIZE,
+					top + BORDER_Y + (i / ITEMS_PER_ROW) * ITEM_SIZE, ITEM_SIZE, ITEM_SIZE, stack));
+			i = (i + 1) % ITEMS_PER_PAGE;
 		}
+
+		next = new ImageButton(left + X_SIZE / 2 + 20, bottom - 8 - BUTTON_SIZE, BUTTON_SIZE, BUTTON_SIZE, 0, 0,
+				0, BUTTON, (b) -> {
+					if ((page + 1) * ITEMS_PER_PAGE < buttons.size())
+						page++;
+				});
+
+		prev = new ImageButton(left + X_SIZE / 2 - 20 - BUTTON_SIZE, bottom - 8 - BUTTON_SIZE, BUTTON_SIZE,
+				BUTTON_SIZE, 0, 0, 0, BUTTON, (b) -> {
+					if (page > 0)
+						page--;
+				});
 	}
 
 	@Override
 	public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
 		if (isActive) {
-			Minecraft.getInstance().getTextureManager().bindTexture(GUI);
+			Minecraft mc = Minecraft.getInstance();
+			mc.getTextureManager().bindTexture(GUI);
 			blit(matrixStack, (width - X_SIZE) / 2 - X_OFFSET, (height - Y_SIZE) / 2, 0, 0, X_SIZE, Y_SIZE);
 
-			for (ItemButton b : buttons)
-				b.render(matrixStack, mouseX, mouseY, partialTicks);
+			for (int i = page * ITEMS_PER_PAGE; i < Math.min(buttons.size(), (page + 1) * ITEMS_PER_PAGE); i++)
+				buttons.get(i).render(matrixStack, mouseX, mouseY, partialTicks);
+
+			next.render(matrixStack, mouseX, mouseY, partialTicks);
+			prev.render(matrixStack, mouseX, mouseY, partialTicks);
+
+			// Draw page number
+			String pageText = (page + 1) + "/" + (buttons.size() / ITEMS_PER_PAGE + 1);
+			int textWidth = mc.fontRenderer.getStringWidth(pageText);
+			mc.fontRenderer.drawString(matrixStack, pageText,
+					(width - X_SIZE) / 2f - X_OFFSET + X_SIZE / 2 - textWidth / 2f,
+					(height - Y_SIZE) / 2f + Y_SIZE - 17, -1);
+
 		}
 	}
 
 	@Override
 	public boolean mouseClicked(double mouseX, double mouseY, int button) {
-		for (ItemButton b : buttons)
-			if (b.mouseClicked(mouseX, mouseY, button))
+		for (int i = page * ITEMS_PER_PAGE; i < Math.min(buttons.size(), (page + 1) * ITEMS_PER_PAGE); i++)
+			if (buttons.get(i).mouseClicked(mouseX, mouseY, button))
 				return true;
+
+		if (next.mouseClicked(mouseX, mouseY, button))
+			return true;
+		if (prev.mouseClicked(mouseX, mouseY, button))
+			return true;
 
 		return false;
 	}
